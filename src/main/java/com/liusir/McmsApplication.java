a19dae60e408c266121e305b3b9e531c3af62065
liuss <lssemail@126.com>
2018-11-27 09:51:09 +0800

    init project

---
+++ src/main/java/com/liusir/McmsApplication.java
@@ -0,0 +1,12 @@
+package com.liusir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McmsApplication.class, args);
+	}
+}
